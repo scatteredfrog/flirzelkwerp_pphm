fde9b5b519b7d1682decf549b5adaa67bb25ac17
Sean Courtney <seancourtney@fab4it.com>
2014-05-19 08:07:18 -0500

    Added garden_services.php

---
+++ garden_services.php
@@ -0,0 +1,122 @@
+<html><!-- #BeginTemplate "/Templates/template0.dwt" -->
+<head>    
+<!-- #BeginEditable "doctitle" --> 
+<title>Portage Park Handyman - Services</title>
+<!-- #EndEditable --> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="Handyman Chicago Portage Park Repair Remodeling Home Repair Improvement Richard Escallier">
+<link rel="stylesheet" type="text/css" href="rich.css">
+<link rel="stylesheet" type="text/css" href="css/garden_services.css">
+</head>
+<script src="js/jquery-1.8.0.min.js"></script>
+<script src="js/angular.min.js"></script>
+<script src="js/angular/GardenCtrl.js"></script>
+<body bgcolor="#CCCCCC" ng-app='Gardening'>
+ 
+<p>&nbsp;</p>
+<table width="800" border="0" cellspacing="2" cellpadding="2" align="center" bgcolor="#FFFFFF">
+<!--BEGINNING OF BODY-->
+<table align="center" bgcolor="#FFFFFF" ng-controller='GardenCtrl'>
+    <tr>
+        <td valign="top"><table width="192"> <!-- BEGIN LINK TABLE W/LOGO -->
+        <td><img src="images/logo_trans_r1_c1.gif"></img><br />
+        <? include "links.html"; ?>
+        </td></table><!-- END LINK TABLE W/LOGO -->
+            <td valign="top"><table width="608" height="118"> <!-- BEGIN TABLE ON RIGHT-->
+        <td valign="middle">
+            <p align="center"><span class="italic">Rich and Judy offer prompt, 
+              professional, <br />efficient, and reasonably-priced solutions <br />to home 
+              repairs and remodeling.</p>
+        </td>
+        <td valign="top"><img src="images/cartoon.gif" width="140" height="166"></td></tr>
+                <!-- END TABLE ON RIGHT-->
+<!-- PUT THE BODY STUFF HERE -->
+          <tr><td id="kwerp">
+                <h3>Gardens by Portage Park Services</h3>
+                <br />
+                <div class="prehead">
+                    Would you like to enhance your curb appeal, the enjoyment of your garden, and 
+                    increasing your property value? It's the &num;2 home improvement that pays off. 
+                    Let Gardens by Portage Park Services help. We offer a variety of services for 
+                    you to choose from. Give us a call today.
+                </div>
+                <br />
+                <div id="tab_container">
+                    <div class="tab_spacer"></div><div class="tab" ng-click="focusTab('design')" id="design"><span class='tab_name'>Garden Design</span></div>
+                    <div class="tab_spacer"></div><div class="tab tab_back tab_wide" ng-click="focusTab('consultation')" id="consultation"><span class='tab_name'>Garden Consultation</span></div>
+                    <div class="tab_spacer"></div><div class="tab tab_back" ng-click="focusTab('coach')" id="coach"><span class='tab_name'>Garden Coach</span></div>
+                    <div class="tab_spacer"></div><div class="tab tab_back tab_wide" ng-click="focusTab('maintenance')" id="maintenance"><span class='tab_name'>Garden Maintenance</span></div>
+                </div>
+                &nbsp;<br />
+                <div class='tab_text' ng-show="gtab=='design'">
+                    <dt>Garden Design (and installation at most sites)</dt>
+                    <div class="prehead">
+                        This option is for gardens in need of a design and/or installation,
+                        whether you're starting with a blank canvas or in need of renovation. 
+                        I will meet with you and find out about your style, your needs and 
+                        wishes for your garden, and how you plan to use it. This will enable 
+                        me to design a garden that meets your needs and one that will be 
+                        enjoyable for years to come.
+                    </div>
+                </div>
+                <div class='tab_text' ng-show="gtab=='consultation'">
+                    <dt>Garden Consultation</dt>
+                    <div class="prehead">
+                        This option is for homeowners who already have an established
+                        garden and may want additional plantings, maintenance, some 
+                        direction, or advice. I will visit your home, charging an hourly 
+                        garden consultation fee. At this time I would listen to your 
+                        concerns and wishes for your space, offer suggestions, and answer 
+                        your questions. What will you come away with? A list of suggestions 
+                        outlined in a way that may enable you to implement the changes 
+                        yourself, or an estimate on any tasks you want further 
+                        assistance with.
+                    </div>
+                </div>
+                <div class='tab_text' ng-show="gtab=='coach'">
+                    <dt>Garden Coach</dt>
+                    <div class="prehead">
+                        This option is for homeowners who enjoy working in their gardens 
+                        and may want to work hand in hand alongside a horticulturist in 
+                        order to enhance their space and learn while doing so. We can 
+                        help you start a herb or vegetable garden; a child's garden with a 
+                        place to plant, dig, and play; or a flower bed -- with front yard and 
+                        back yard assistance. Want to learn how and when to prune your shrubs, 
+                        how to sharpen your garden tools, or how to test and amend your soil? 
+                        Are you interested in starting a compost bin, or are you having issues 
+                        with an existing bin? Are you interested in vermicompost? Plants need 
+                        to be moved or divided? You can tell me what's important to you and 
+                        what you hope to gain or learn while working with a garden coach.
+                    </div>
+                </div>
+                <div class='tab_text' ng-show="gtab=='maintenance'">
+                    <dt>Garden Maintenance</dt>
+                    <div class="prehead">
+                        <ul>
+                            <li>Frequency of visits determined based on your needs and budget.</li>
+                           <li>Maintain cleanliness of planters.</li>
+                           <li>Monitor, evaluate and treat any disease and/or pest conditions.</li>
+                           <li>Perform any necessary pruning and deadheading, remove old foliage around plants.</li>
+                           <li>Remove accumulated debris from planting beds and landscape areas.</li>
+                           <li>Planting bed outline to be redefined as needed.</li>
+                           <li>Edging along sidewalk/lawn as needed.</li>
+                           <li>Remove weeds in planting beds.</li>
+                           <li>Soil and mulch can be provided at an additional cost.</li>
+                        </ul>
+                    </div>
+                </div>
+              </td></tr>
+
+          
+    
+<!-- END OF BODY STUFF -->
+</table>
+    </tr>
+</table>
+
+</table>
+</table>
+</body>
+</html>
+
+<!--END OF BODY-->
--- services.php
+++ services.php
@@ -29,117 +29,157 @@
                 <!-- END TABLE ON RIGHT-->
 <!-- PUT THE BODY STUFF HERE -->
           <tr><td>
-                  <h3>Services</h3>
-                  <dl>
-<dt>Landscape</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-        <li style="width:550px;">Improving curb appeal</li>
-        <li style="width:550px;">Garden spruce-up to full design services</li>
-        <li style="width:550px;">Seasonal containers to enhance your entryway or patio</li>
-        <li style="width:550px;">Pruning, dividing perennials, shaping planting beds, adding mulch</li>
-        <li style="width:550px;">Other suggestions or garden lessons to make the best use of your current plants</li>
-        <li style="width:550px;">Phase things in if your budget prefers</li>
-    </ul><br />
-<dt>Organizing</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Kitchen, laundry room, basement, garage, whole house</li>
-    <li style="width:550px;">Get the best use of the space you have</li>
+                <h3>Services</h3>
+                <div class='prehead'>
+                We've been in business for more than ten years, with over 1,000 satisfied customers. 
+                We don't try to do everything, but we enjoy doing the work we accept and are 
+                confident in our ability to make things better for our customers.<br />&nbsp;<br />
+                Judy is very organized. Her spice cabinet is alphabetized, and she always knows where 
+                things are. She can help you with organizing, running errands, or taking care of the 
+                necessary details, big or small, that can help you to get caught up.<br />&nbsp;<br />
+                Call today and let us help with your to-do list.<br />&nbsp;<br />
+                If you need help with something that you don't see on these lists, please ask; chances 
+                are we can do it.<br />&nbsp;<br />
+                </div>
+                <dl>
+<dt>Help Organizing the House</dt>
+<div class='prehead'>
+    We can help you finally get your home in order. Have you tried more than once but
+    been unable to finish or gotten sidetracked? Let us take care of it for you.
+</div>
+<ul class="services">
+    <li>Kitchens</li>
+    <li>Closets</li>
+    <li>Bedrooms</li>
+    <li>Home offices</li>
+    <li>Garage</li>
+    <li>Basement</li>
+    <li>Build shelving</li>
+    <li>Get rid of unwanted goods</li>
 </ul><br />
+<dt>Errand Service</dt>
+<ul class="services">
+    <div class="prehead">
+        Want to relax after working a full day? Let us take care of your errands quickly 
+        and efficiently, leaving you with more time for the things you enjoy.
+    </div>
+        <li>Dry cleaning pickup / dropoff</li>
+        <li>Prescription pickup / dropoff</li>
+        <li>Post office deliveries</li>
+        <li>Deliver or pick up personal items</li>
+    </ul><br />
+<dt>At Home</dt>
+<ul class="services">
+        <li>Wait in home for repairmen, technicians, and package deliveries</li>
+        <li>Water plants while you are away</li>
+        <li>Watch your home during remodeling / construction</li>
+        <li>Ensure your home is safe for your children to arrive from school</li>
+    </ul><br />
+<dt>Landscape</dt>
+<ul class="services">
+        <li>Improving curb appeal</li>
+        <li>Garden spruce-up to full design services</li>
+        <li>Seasonal containers to enhance your entryway or patio</li>
+        <li>Pruning, dividing perennials, shaping planting beds, adding mulch</li>
+        <li>Other suggestions or garden lessons to make the best use of your current plants</li>
+        <li>Phase things in if your budget prefers</li>
+    </ul><br />
 <dt>Security</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Change locks and deadbolts (having only one key is nice)</li>
-    <li style="width:550px;">Please see why <a href="security.php">these locks</a> are on our home!</li>
+<ul class="services">
+    <li>Change locks and deadbolts (having only one key is nice)</li>
+    <li>Please see why <a href="security.php">these locks</a> are on our home!</li>
 </ul>
 <br />
 <dt>Drywall</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Patch, repair, and replace existing drywall</li>
+<ul class="services">
+    <li>Patch, repair, and replace existing drywall</li>
 </ul>
 <br />
 <dt>Doors and Windows</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Help doors that need a hip check to close, two hands to open, or go "<span style='font-style:italic;'>squeak squeak squeak</span>"</li>
-    <li style="width:550px;">Install or adjust bi-fold doors</li>
-    <li style="width:550px;">Replace broken ropes with chains</li>
-    <li style="width:550px;"><a href="security.php">Secure your garage service door</a>, prevent it from being kicked in</li>
+<ul class="services">
+    <li>Help doors that need a hip check to close, two hands to open, or go "<span style='font-style:italic;'>squeak squeak squeak</span>"</li>
+    <li>Install or adjust bi-fold doors</li>
+    <li>Replace broken ropes with chains</li>
+    <li><a href="security.php">Secure your garage service door</a>, prevent it from being kicked in</li>
 </ul>
 <br />
 <dt>Kitchens</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Backsplashes</li>
-    <li style="width:550px;">Cabinet door adjustments or repairs</li>
-    <li style="width:550px;">Garbage disposals: rescue, replace, or install</li>
-    <li style="width:550px;">Faucets replaced</li>
+<ul class="services">
+    <li>Backsplashes</li>
+    <li>Cabinet door adjustments or repairs</li>
+    <li>Garbage disposals: rescue, replace, or install</li>
+    <li>Faucets replaced</li>
 </ul>
 <br />
 <dt>Baths</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Straighten, tighten, or install towel bars, robe hooks, or accessories</li>
-    <li style="width:550px;">Grab bars installed</li>
-    <li style="width:550px;">Faucets or vanities replaced</li>
+<ul class="services">
+    <li>Straighten, tighten, or install towel bars, robe hooks, or accessories</li>
+    <li>Grab bars installed</li>
+    <li>Faucets or vanities replaced</li>
 </ul>
 <br />
 <dt>Storage</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Closets, garages, or odd little spaces</li>
-    <li style="width:550px;">Shelving</li>
+<ul class="services">
+    <li>Closets, garages, or odd little spaces</li>
+    <li>Shelving</li>
 </ul>
 <br />
 <dt>Electrical</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Light fixtures, ceiling fans, dimmers, general troubleshooting</li>
+<ul class="services">
+    <li>Light fixtures, ceiling fans, dimmers, general troubleshooting</li>
 </ul>
 <br />
 <dt>Plumbing<!-- * --></dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Fix leaks</li>
-    <li style="width:550px;">Repair walls after leaks</li>
+<ul class="services">
+    <li>Fix leaks</li>
+    <li>Repair walls after leaks</li>
 </ul><!-- <span style="font-size:12px; font-style:italic;">* Plumbing services are not available in high-rises.</span><br />
 &nbsp; --><br />
 <dt>Carpentry</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Hand rails, stripped screws</li>
-    <li style="width:550px;">Minor carpentry repairs</li>
+<ul class="services">
+    <li>Hand rails, stripped screws</li>
+    <li>Minor carpentry repairs</li>
 </ul>
 <br />
 <dt>Filter changes</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Furnace, humidifier, refrigerator</li>
-    <li style="width:550px;">With each season or one-time</li>
-    <li style="width:550px;">Clean filters make your furnace more efficient 
+<ul class="services">
+    <li>Furnace, humidifier, refrigerator</li>
+    <li>With each season or one-time</li>
+    <li>Clean filters make your furnace more efficient 
         (better air flow = lower utility bills and more home comfort</li>
 </ul>
 <br />
 <dt>New home owner package</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Change locks</li>
-    <li style="width:550px;">Change filters</li>
-    <li style="width:550px;">Walkthrough to evaluate your home's systems "bones" - point out 
+<ul class="services">
+    <li>Change locks</li>
+    <li>Change filters</li>
+    <li>Walkthrough to evaluate your home's systems "bones" - point out 
         urgencies, suggest priorities, discuss your wish list and possibilities, plan</li>
-    <li style="width:550px;">The remainder of the day working on the items of your choice</li>
-    <li style="width:550px;">Best value: have the Handyman and the Handyma'am for a full day</li>
+    <li>The remainder of the day working on the items of your choice</li>
+    <li>Best value: have the Handyman and the Handyma'am for a full day</li>
 </ul>
 <br />
 <dt>Off-hour emergency services available (if possible) for established customers only</dt>
 <br />
 <dt>Gift Services</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">New homeowner package for yourself or a friend</li>
-    <li style="width:550px;">Tool Box Shopping Service. Based on the recipient's 
+<ul class="services">
+    <li>New homeowner package for yourself or a friend</li>
+    <li>Tool Box Shopping Service. Based on the recipient's 
         tool collection, we can suggest, purchase, and deliver quality hand tools 
         once or every holiday</li>
 </ul>
 <br />
 <dt>Miscellaneous</dt>
-<ul style="margin:5px 12px; padding:5px 12px;"><li style="width:550px;">Don't see what you're looking for? Please 
+<ul class="services">
+    <li>Don't see what you're looking for? Please 
         <a href="mailto:handyman4chicago@aol.com?subject=Your services">contact us</a>. Asking is free!</li></ul>
 <br />
 <dt>Things we <span style="font-style: italic;">don't</span> do</dt>
-<ul style="margin:5px 12px; padding:5px 12px;">
-    <li style="width:550px;">Take on a job that is beyond our skill set</li>
-    <li style="width:550px;">Plumbing jobs in high rises</li>
-    <li style="width:550px;">Electric jobs involving cloth-covered (brittle, old) wires</li>
-    <li style="width:550px;">Furnace problems that are deeper than the filter</li>
+<ul class="services">
+    <li>Take on a job that is beyond our skill set</li>
+    <li>Plumbing jobs in high rises</li>
+    <li>Electric jobs involving cloth-covered (brittle, old) wires</li>
+    <li>Furnace problems that are deeper than the filter</li>
 </ul>
                   </dd>
                   </dl>
