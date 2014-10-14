e25864e826ee2834be3400d57fc831d35570b1f5
Sean Courtney <seancourtney@fab4it.com>
2014-10-14 13:52:13 -0500

    How green is your handyman?

---
+++ how_green.php
@@ -0,0 +1,102 @@
+<html><!-- #BeginTemplate "/Templates/template0.dwt" -->
+<head>    
+<!-- #BeginEditable "doctitle" --> 
+<title>Portage Park Handyman - How Green is Your Handyman?</title>
+<!-- #EndEditable --> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="Handyman Chicago Portage Park Repair Remodeling Home Repair Improvement Richard Escallier">
+<link rel="stylesheet" type="text/css" href="rich.css">
+<link rel="stylesheet" type="text/css" href="css/how_green.css">
+</head>
+<body bgcolor="#CCCCCC" ng-app='Gardening'>
+<script src="js/jquery-1.8.0.min.js"></script>
+ 
+<p>&nbsp;</p>
+<table width="800" border="0" cellspacing="2" cellpadding="2" align="center" bgcolor="#FFFFFF">
+<!--BEGINNING OF BODY-->
+<table align="center" bgcolor="#FFFFFF" ng-controller='GardenCtrl'>
+    <tr>
+        <td valign="top"><table width="192"> <!-- BEGIN LINK TABLE W/LOGO -->
+        <td><img src="images/logo_trans_r1_c1.gif"></img><br />
+        <?php include "links.html"; ?>
+        </td></table><!-- END LINK TABLE W/LOGO -->
+            <td valign="top"><table width="608" height="118"> <!-- BEGIN TABLE ON RIGHT-->
+        <td valign="middle">
+            <p align="center"><span class="italic">Rich and Judy offer prompt, 
+              professional, <br />efficient, and reasonably-priced solutions <br />to home 
+              repairs and remodeling.</p>
+        </td>
+        <td valign="top"><img src="images/cartoon.gif" width="140" height="166"></td></tr>
+                <!-- END TABLE ON RIGHT-->
+<!-- PUT THE BODY STUFF HERE -->
+          <tr><td id="kwerp">
+                <h3>How Green is Your Handyman?</h3>
+                <br />
+                <div class="prehead">
+                    I strongly believe that keeping our environment clean and healthy is 
+                    everybody's responsibility, and I practice what I preach. Here are just
+                    a few ways that Portage Park Handyman keeps the ecology in mind.
+                </div>
+                <br />
+                <div class='greenDiv'>
+                    <dt class="greenPrehead">Too Much Cement? No Such Thing!</dt>
+                    <div class="imageContainer" style="background-image: url(images/how_green/repaired_stairs.jpg);" >
+                    </div>
+                    <div class="prehead">
+                        While doing a minor cement repair, I had mixed a full bag of cement when 
+                        a quarter of a bag would have sufficed. I was cleaning up in the alley and 
+                        about to throw the excess in the garbage -- and my <span style="font-style: italic;">
+                        That's not very green!</span> alarm went off. Fortunately, so did the 
+                        light bulb over my head.
+                        <br />
+                    </div>
+                </div>
+                <div class="greenDiv">
+                    <div class="imageContainer" style="background-image:url(images/how_green/alley_pothole.jpg);">
+                    </div>
+                    <div class="prehead">
+                        After the brutal winter of 2013-2014, I didn't 
+                        have to walk down the street more than 25 feet before I found a pothole that 
+                        had no trouble swallowing up half a bag of cement.
+                    </div>
+                </div>
+                <div class='greenDiv'>
+                    <dt class="greenPrehead">Staying Sharp</dt>
+                    <div class="imageContainer" style="background-image: url(images/how_green/carpenter_pencil.jpg);" >
+                    </div>
+                    <div class="prehead">
+                        When I sharpen my carpenter's pencil, I do so over the compost 
+                        container. The shavings become mulch.
+                    </div>
+                </div>
+                <div class='greenDiv'>
+                    <dt class="greenPrehead">Rebuilding Exchange</dt>
+                    <div class="prehead">
+                        When I install new items for cosmetic preference, I donate a 
+                        functional fixture or faucet to 
+                        <a href="http://rebuildingexchange.org" target="_blank">
+                            Rebuilding Exchange.
+                        </a>
+                    </div>
+                </div>
+                <div class='greenDiv'>
+                    <dt class="greenPrehead">No Parts Left Unused</dt>
+                    <div class="prehead">
+                        Any leftover metal is left for the scrappers to collect and use.
+                    </div>
+                </div>
+              </td></tr>
+
+          
+    
+<!-- END OF BODY STUFF -->
+</table>
+    </tr>
+</table>
+
+</table>
+</table>
+</body>
+</html>
+
+<!--END OF BODY-->
