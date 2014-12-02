12170339d108c91220d627ca9c4ce9a435d03fb6
Sean Courtney <seancourtney@fab4it.com>
2014-12-02 13:34:18 -0600

    PPHM-003 Initial commit
    Conflicts:
    
    	images/.DS_Store
    	nbproject/private/private.xml

---
+++ garden_photos.php
@@ -0,0 +1,46 @@
+<?php
+?>
+<html><!-- #BeginTemplate "/Templates/template0.dwt" -->
+    <head>
+        <!-- #BeginEditable "doctitle" --> 
+        <title>Expert Chicago Handyman Services: Portage Park Handyman - Home</title>
+        <!-- #EndEditable --> 
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <meta name="google-site-verification" content="Re60l-vAPeDIvICQ4oPnDM4IgPYvPDwIu-HEyyTFwbY" />
+        <meta name="msvalidate.01" content="BD92026BC398D5BD8CF1F3F83BC3A121" />
+        <meta name="keywords" content="Handyman Chicago Portage Park Repair Remodeling Home Repair Improvement Richard Escallier">
+        <link rel="stylesheet" type="text/css" href="rich.css" />
+        <link rel="stylesheet" type="text/css" href="css/fotorama.css" />
+    </head>
+
+    <body bgcolor="#CCCCCC">
+        <script src="js/jquery-1.8.0.min.js"></script>
+        <script src="js/fotorama.js"></script>
+        
+        <div class="fotorama" data-nav="thumbs" data-fit="contain" data-width="640" data-height="640">
+            <img src="images/garden/IMG_1213_4.jpg" data-caption="<span class='pp_cap'><span class='pp_cap_head'>Parkway Plantings</span><br />Good example of blending textures<br /><span class='pp_cap_foot'>Yellow Sundrops</span></span>" />
+            <img src="images/garden/IMG_1214_4.jpg" data-caption="<span class='pp_cap'>Parkway garden</span>"/>
+            <img src="images/garden/IMG_1215_4.jpg" />
+            <img src="images/garden/IMG_1216_4.jpg" />
+            <img src="images/garden/IMG_1217_4.jpg" />
+            <img src="images/garden/IMG_1218_4.jpg" />
+            <img src="images/garden/IMG_1219_4.jpg" />
+            <img src="images/garden/IMG_1222_4.jpg" />
+            <img src="images/garden/IMG_1223_4.jpg" />
+            <img src="images/garden/IMG_1224_4.jpg" />
+            <img src="images/garden/IMG_1225_4.jpg" />
+            <img src="images/garden/IMG_1226_4.jpg" />
+            <img src="images/garden/IMG_1227_4.jpg" />
+            <img src="images/garden/IMG_1232_4.jpg" />
+            <img src="images/garden/IMG_1233_4.jpg" />
+            <img src="images/garden/IMG_1234_4.jpg" />
+            <img src="images/garden/IMG_1235_4.jpg" />
+            <img src="images/garden/IMG_1236_4.jpg" />
+            <img src="images/garden/IMG_1237_4.jpg" />
+            <img src="images/garden/IMG_1238_4.jpg" />
+            <img src="images/garden/IMG_1239_4.jpg" />
+            <img src="images/garden/IMG_1240_4.jpg" />
+            <img src="images/garden/IMG_1241_5.jpg" />
+        </div>
+    </body>
+</html>
