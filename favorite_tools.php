adf112e7cd67ca11d7c3dbce493b2ad266511d05
Sean Courtney <seancourtney@fab4it.com>
2015-03-28 07:50:28 -0500

    PPHM-010 recommended tools

---
+++ favorite_tools.php
@@ -0,0 +1,306 @@
+<?php
+    error_log("Server info:".print_r($_SERVER,1));
+    if (substr($_SERVER['HTTP_HOST'],0,5) === 'local') {
+        $db_address = '127.0.0.1';
+        $db_user_name = 'root';
+        $db_user_password = 'sqflirzel';
+    } else {
+        $db_address = 'db401201757.db.1and1.com';
+        $db_user_name = 'db401201757';
+        $db_user_password = 'pphmdb42';
+    }
+?>
+<html><!-- #BeginTemplate "/Templates/template0.dwt" -->
+<head>    
+<!-- #BeginEditable "doctitle" --> 
+<title>Portage Park Handyman - Handyman's Toolbox</title>
+<!-- #EndEditable --> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="keywords" content="Handyman Chicago Portage Park Repair Remodeling Home Repair Improvement Richard Escallier">
+<link rel="stylesheet" type="text/css" href="rich.css">
+<link rel="stylesheet" type="text/css" href="css/toolsmats.css">
+<link rel="stylesheet" type="text/css" href="css/fotorama.css">
+<script src='js/jquery-1.8.0.min.js'></script>
+<script src='js/fotorama.js'></script>
+<script src="js/angular.min.js"></script>
+<script src="js/angular/ToolsMatsCtrl.js"></script>
+<script src="js/toolsmats.js"></script>
+</head>
+
+<body bgcolor="#CCCCCC" ng-app="ToolsMats">
+<?php
+    $link=mysql_connect($db_address, $db_user_name, $db_user_password);
+    if(!$link) {
+      die('Count not connect: '.mysql_error());
+    }
+    mysql_select_db("db401201757") or die(mysql_error());
+
+    $item = mysql_query("SELECT id,heading,mnemonic,description,sort_order,amazon FROM rec_tools");
+?>
+
+<p>&nbsp;</p>
+<table width="800" border="0" cellspacing="2" cellpadding="2" align="center" bgcolor="#FFFFFF">
+<!--BEGINNING OF BODY-->
+<table align="center" bgcolor="#FFFFFF" ng-controller="ToolsMatsCtrl">
+    <tr>
+        <td valign="top"><table width="192"> <!-- BEGIN LINK TABLE W/LOGO -->
+        <td><img src="images/logo_trans_r1_c1.gif"></img><br />
+        <?php include "links.html"; ?>
+        </td></table><!-- END LINK TABLE W/LOGO -->
+            <td valign="top"><table width="608" height="118"> <!-- BEGIN TABLE ON RIGHT-->
+        <td valign="middle">
+            <p align="center"><span class="italic">Rich and Judy offer prompt, 
+              professional, <br />efficient, and reasonably-priced solutions <br />to home 
+              repairs and remodeling.</p>
+        </td>
+        <td valign="top"><img src="images/cartoon.gif" width="140" height="166"></td></tr>
+                <!-- END TABLE ON RIGHT-->
+<!-- PUT THE BODY STUFF HERE -->
+          <tr><td>
+                <h3>Handyman's Favorite Tools</h3>                
+                <div class='prehead'>
+                    These are the tools that I own and use every day on home repairs
+                    all around the city.</div>
+                <br />&nbsp;<br />                
+                    <?php
+                        while ($row=mysql_fetch_array($item)) { ?>
+                <div id="worm-fact" class="sale saleTitle">
+                        <?= $row['heading']; ?>
+                </div>
+                <div class="saleOtherStuff">
+                    <div class="sale saleDescription">
+                        <?= $row['description']; ?>
+                    </div>
+                    <div>
+                        <?= $row['amazon']; ?>
+                    </div>
+                        <?php }
+                ?>
+
+                    <a name="wfact">Worm Factory 360 Composting Worm Bin</a>
+                </div>
+                       The Worm Factory 360 composter uses worms to break down organic 
+                        waste. Its compact design makes it perfect to use in any 
+                        household.<br />
+                        It is easy to recycle kitchen scraps and paper into nutrient-rich 
+                        fertilizer for your plants.<br />
+                        Managing the Worm Factory 360 takes less than 15 minutes a week. 
+                        Add worms and your organic waste to the bottom tray. Once that tray 
+                        is filled add another. The worms migrate upward leaving their 
+                        compost behind. It works year-round inside or outside.<br />
+                        The finished compost can be used on any plants.<br />
+                        To see the available products or to order, <a href="worm_order.php">please click here</a>.<br />
+                        <span ng-click="arrowClick('worm-fact')" id="worm-fact-see" class="clickToSee">Click here to see pictures and details.</span>
+                    </div>
+                    <div ng-show="id=='worm-fact'" id="worm-fact-pix" class="fotorama" data-width="408" data-fit="cover">
+                        <img src="images/sale/worm_bin/Cutaway-1B.jpg" />
+                        <img src="images/sale/worm_bin/Cutaway-1G.jpg" />
+                        <img src="images/sale/worm_bin/Cutaway-1T.jpg" />
+                        <img src="images/sale/worm_bin/Cutaway-1B.jpg" />
+                        <img src="images/sale/worm_bin/Lifestyle-1B.jpg" />
+                        <img src="images/sale/worm_bin/Color-Options.jpg" />
+                        <img src="images/sale/worm_bin/Lifestyle-2B.jpg" />
+                        <img src="images/sale/worm_bin/Breakout-1B.jpg" />
+                        <img src="images/sale/worm_bin/Lifestyle-3B.jpg" />
+                    </div>
+                    <div class="sale saleDescription" id="worm_description">
+                        <dt>Features</dt>
+                        <ul>
+                            <li>Compact and expandable up to  trays</li>
+                            <li>US made high quality post consumer recycled plastic</li>
+                            <li>Choice of color: Black, green, terracotta</li>
+                            <li>Create compost year-round, indoors and out</li>
+                            <li>10 year limited warranty</li>
+                            <li>Odor free operation</li>
+                            <li>Houses thousands of worms that consume pounds of waste per week</li>
+                        </ul>
+                        <dt>What's Included</dt>
+                        <ul>
+                            <li>"Quick Tips" instructions on lid</li>
+                            <li>Improved base and lid design for better air circulation</li>
+                            <li>Comprehensive instruction booklet</li>
+                            <li>4 chapter DVD guide for beginners</li>
+                            <li>Start-up worm bedding</li>
+                            <li>Accessory kit includes: thermometer, hand rake, and scraper</li>
+                            <li>Built in leachate (liquid fertilizer) collector tray and spigot for easy draining</li>
+                            <li>Glacial rock dust</li>
+                        </ul>
+                        <dt>Technical Details/Specifications</dt>
+                        <span class='bold'>Green</span>
+                        <ul>
+                            <li>Shipping size: 18" x 18" x 15"</li>
+                            <li>Full expansion: 18" x 18" x 24"</li>
+                            <li>Shipping weight: 13 Pounds</li>
+                            <li>UPC: 043433944602</li>
+                            <li>SKU: WF360 Green</li>
+                            <li>Our Price: $109.95 <span class="bin" onclick="buyThisNow('nb_wf360_green')">Buy This Now</span></li>
+                        </ul>
+                        <span class='bold'>Black</span>
+                        <ul>
+                            <li>Shipping size: 18" x 18" x 15"</li>
+                            <li>Full expansion: 18" x 18" x 24"</li>
+                            <li>Shipping weight: 13 Pounds</li>
+                            <li>UPC: 043433944503</li>
+                            <li>SKU: WF360 Black</li>
+                            <li>Our Price: $109.95 <span class="bin" onclick="buyThisNow('nb_wf360_black')">Buy This Now</span></li>
+                        </ul>
+                        <span class='bold'>Terracotta</span>
+                        <ul>
+                            <li>Shipping size: 18" x 18" x 15"</li>
+                            <li>Full expansion: 18" x 18" x 24"</li>
+                            <li>Shipping weight: 13 Pounds</li>
+                            <li>UPC: 043433944701</li>
+                            <li>SKU: WF360 Terra</li>
+                            <li>Our Price: $109.95 <span class="bin" onclick="buyThisNow('nb_wf360_tc')">Buy This Now</span></li>
+                        </ul>
+                        <dt>Testimonials</dt>
+                        <ul>
+                            <li>"It's a wonder using the Worm Factory to teach kids that most of what they have in life can be
+recycled." -- Susan W., Poplar Road Elementary</li>
+                            <li>"This product is a wonderful addition to anyone practicing organic gardening. The worm castings
+are easy to harvest and are a priceless addition to flower pots." -- Gretchen B., National Gardening Club</li>
+                            <li>"Absolutely no smell or mess. I have mine in the kitchen! It's great to have the worms at work all
+                                winter in NE New York, and not have to schlep the compost to the pile, awaiting the spring thaw. I love this product!" -- K. Kotarski, National Gardening Club</li>
+                            <li>"I have had the product working for a couple of months now and love it. Based on design, ease of use and appearance, I have recommended this product to several fellow gardeners. I have
+always thought I did a good job recycling and composting, but this has raised that to a new level and I now use items such as newspaper and paper towels for my worms that would either been
+recycled or thrown in the trash. Coming up with food "recipes" is quite fun." -- Sue F.</li>
+                        </ul>
+                        <img src="images/sale/worm_bin/Support-2.jpg" width="145" height="109" />
+                        <img src="images/sale/worm_bin/Support-3.jpg" width="145" height="148" />
+                        <img src="images/sale/worm_bin/Support-1.jpg" width="145" height="107" /><br />
+                        &nbsp;<br />
+                        <dt>Companion Products / Accessories</dt>
+                        <div id="support_container" style="padding-bottom:5px;">
+                            <div class="quarterSpace">
+                                <span class="bold">Worms</span><br />
+                                <span class="fshrink">Essential for worm composting</span>
+                            </diV>
+                            <div class="quarterSpace">
+                                <span class="bold">Extra trays</span><br />
+                                <span class="fshrink">Increase your production by expanding your number of processing trays</span>
+                            </div>
+                            <div class="quarterSpace">
+                                <span class="bold">Coir</span><br />
+                                <span class="fshrink">Ideal worm bedding</span>
+                            </div>
+                            <div class="quarterSpace">
+                                <span class="bold">Pumice</span><br />
+                                <span class="fshrink">Ideal worm bedding</span>
+                            </div>
+                        </div>
+                        <span ng-click="arrowClick('worm-fact')" class="clickToSee">Click here to hide pictures and details.</span>
+                    </div>                    
+                    &nbsp;<br />
+                </div>
+                <div id="litter" class="sale saleTitle">
+                    Escape From the Landfill: Cat Litter Containers
+                </div>
+                <div class="saleOtherStuff">
+                    <div class="sale saleDescription">
+                        Keeping in mind <a href="how_green.php">the importance of being
+                        green</a>, I have repurposed cat litter buckets that can be
+                        used for storing rock salt, play sand, indoor plants, or anything 
+                        else you need to store for easy access. These containers have 
+                        nice artwork stickers (sold separately for $6 each) to cover the 
+                        original cat litter verbiage and provide a more pleasing design.<br />
+                        Price: $8.00.<br />
+                        <span ng-click="arrowClick('litter')" id="litter-see" class="clickToSee">Click here to see pictures.</span>
+                    </div>
+                    <div ng-show="id=='litter'" id="litter-pix" class="fotorama">
+                        <img src="/images/sale/tcat1.jpg" />
+                        <img src="/images/sale/tcat2.jpg" />
+                        <img src="/images/sale/tcat3.jpg" />
+                    </div>
+                    &nbsp;<br />
+                </div>
+                
+                <div id="covers" class="sale saleTitle">
+                    Half Round Window Covers
+                </div>
+                <div class="saleOtherStuff" id="covers-stuff">
+                    <div class="sale saleDescription">
+                        These covers for half round windows are temporary and removable. 
+                        Having them custom-made and installed would cost hundreds of dollars 
+                        each. Our much more affordable options are made using temporary vinyl 
+                        window coverings from the box stores and have been in use for at least 
+                        four years. These window covers reduce middle-of-summer heat gain but 
+                        do not block out all the light. When winter comes or if you want to 
+                        let in evening light during warmer times of year, the covers are easy to 
+                        remove.<br />
+                        Price: $49 each<br />
+                        $125 to measure, deliver, and install.<br />
+                        Windows pictured are 38 inches in diameter (19 inches tall). When closed, 
+                        the covering sits on the sill and is 1.5 inches by 1.5 inches by 18.5 
+                        inches.<br />
+                        <span ng-click="arrowClick('covers')" id="covers-see" class="clickToSee">Click here to see pictures.</span>
+                    </div>
+                    <div id="covers-pix" class="fotorama">
+                        <img src="/images/sale/hround1.jpg" />
+                        <img src="/images/sale/hround2.jpg" />
+                        <img src="/images/sale/hround3.jpg" />
+                    </div>
+                    &nbsp;<br />
+                </div>
+
+                <div class="sale saleTitle" id="leather">
+                    Vintage Leather Shop collection from the '70s</div>
+                <div id="leather-stuff" class="saleOtherStuff">
+                    <div class="sale saleDescription">
+                        <p>Everything you'd need, including the hides and tutorials, to start and run 
+                            a custom leather shop!</p>
+                        <p>One of the college dorms ran leather workshops next to the ceramics studio. 
+                           I picked up the craft and made a living selling belts, purses, hats, 
+                           wristbands, and a few garments. The money was in making and selling belts and 
+                           wristbands with a name. Carving purses and bags was the artistic side, but 
+                           the time required for the detail that makes a fine bag didn't pay the rent.
+                           Cash in the pocket didn't last long in those days, but Tandy Leather was 
+                           located between work (Village Cab Company in Oak Park) and home. I'd buy a 
+                           leather stamp or carving guide that I didn't have so that someday I may 
+                           create thousands of quality leather items. Turning a hobby into a business 
+                           was not the end of the rainbow; the real job(s) lasted decades. The hobby 
+                           came back every holiday season as special-order named belts and wristbands 
+                           meant very good cheer for both customers and myself for decades.  In the 
+                           '90s the new hobby became home repair, and those tools and materials 
+                           squeezed the leather shop into boxes. I'd pull things out occasionally to 
+                           rivet a carpenter's tool pouch or add a hole in a belt (too much of the  
+                           latter going on). Then one day I went by and thought I heard the tools 
+                           calling me.  When I opened the box they said, "Help! We're trapped in here 
+                           and want to be used! Find us someone who will appreciate our quality and 
+                           vintage character!"</p>
+                        <p>So, the leather workshop is for sale.</p>
+                        <p>Most of the items are still available at 
+                            <a href="http://www.tandyleatherfactory.com" target="_blank">Tandy Leather</a>. 
+                            The following categories at Tandy have many items that are in my collection:
+                        <ul>
+                            <li>Lacing</li>
+                            <li>Leather</li>
+                            <li>Leathercraft Books</li>
+                            <li>Leathercraft Patterns</li>
+                            <li>Leathercraft Projects</li>
+                            <li>Hardware</li>
+                            <li>Stamps</li>
+                            <li>Tools</li>
+                        </ul>
+                        </p>
+                        <p>More than half of the stamps, tools, or craftaids, as well as DIY tutorials that 
+                            worked as a '70s version of YouTube are here waiting for the next leather 
+                            craftsman.
+                        </p>
+                        <p>Retail value of this collection is in the $6k - $7k range.</p>
+                    </div>
+                </div>
+              </td></tr>
+
+          
+    
+<!-- END OF BODY STUFF -->
+</table>
+    </tr>
+</table>
+
+</table>
+
+</body>
+</html>
+
+<!--END OF BODY-->
